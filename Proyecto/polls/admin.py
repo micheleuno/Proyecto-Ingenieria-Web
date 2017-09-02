2ca0e9185f8f8207dd4682cb970783ef45367eb2
Michele <michele.de.conti@hotmail.com>
2017-09-02 10:52:10 -0300

    Update al admin

--- Proyecto/polls/admin.py
+++ Proyecto/polls/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import  Estudiante, Carrera, MallaCurricular, Asignatura, InstanciaAsignatura, EstadoInscripcion, MatriculaMalla, InscripcionAsignatura
 
-admin.site.register(Question)
+
+
+admin.site.register(Estudiante)
+admin.site.register(Carrera)
+admin.site.register(MallaCurricular)
+admin.site.register(Asignatura)
+admin.site.register(InstanciaAsignatura)
+admin.site.register(EstadoInscripcion)
+admin.site.register(MatriculaMalla)
+admin.site.register(InscripcionAsignatura)
